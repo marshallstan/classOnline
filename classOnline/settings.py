70d52d9ca44fd1329e301b371d1bdf71c48d939f
marshallstan <413428648@qq.com>
2019-03-02 21:57:22 +0800

    mysql basic setting

--- classOnline/settings.py
+++ classOnline/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'classOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'classonline',
+        'USER': 'root',
+        'PASSWORD': 'root888',
+        'HOST': '127.0.0.1'
     }
 }
 
