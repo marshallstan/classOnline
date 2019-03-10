e1f0e61c6a1a8a0230acd1c9521e7d2d6d6d2334
marshallstan <413428648@qq.com>
2019-03-10 22:03:34 +0800

    RegisterForm captcha

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 from django.views.generic.base import View
+from django.contrib.auth.hashers import make_password
 
 from .models import UserProfile
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 
 class CustomBackend(ModelBackend):
@@ -16,6 +17,23 @@ class CustomBackend(ModelBackend):
                 return user
         except Exception as e:
             return None
+
+
+class RegisterView(View):
+    def get(self, request):
+        register_form = RegisterForm()
+        return render(request, 'register.html', {'register_form': register_form})
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            user_name = request.POST.get('email', '')
+            pass_word = request.POST.get('password', '')
+            user_profile = UserProfile()
+            user_profile.username = user_name
+            user_profile.email = user_name
+            user_profile.password = make_password(pass_word)
+            user_profile.save()
 
 
 class LoginView(View):
