85743711a698428e34b09aae827c45cc81efcc5d
marshallstan <413428648@qq.com>
2019-03-14 21:56:30 +0800

    def __str__(self)

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -12,6 +12,9 @@ class CityDict(models.Model):
         verbose_name = '城市'
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
+
 
 class CourseOrg(models.Model):
     name = models.CharField(max_length=50, verbose_name='机构名称')
@@ -26,6 +29,9 @@ class CourseOrg(models.Model):
     class Meta:
         verbose_name = '课程机构'
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 class Teacher(models.Model):
@@ -42,3 +48,6 @@ class Teacher(models.Model):
     class Meta:
         verbose_name = '教师'
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
