5dd80cc402b18161b3b30ba4e935d7dfe2b54550
marshallstan <413428648@qq.com>
2019-03-16 22:52:12 +0800

    pagination

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import CourseOrg, CityDict
 
@@ -12,8 +13,17 @@ class OrgView(View):
         all_orgs = CourseOrg.objects.all()
         org_nums = all_orgs.count()
         all_cities = CityDict.objects.all()
+
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_orgs, 2, request=request)
+        orgs = p.page(page)
+
         return render(request, 'org-list.html', {
-            'all_orgs': all_orgs,
+            'all_orgs': orgs,
             'all_cities': all_cities,
             'org_nums': org_nums,
         })
