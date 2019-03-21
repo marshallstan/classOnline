3d406bd270a1e7892308f2d45e6627c106aebcd6
marshallstan <413428648@qq.com>
2019-03-21 19:29:02 +0800

    add_fav

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -2,7 +2,7 @@
 
 from django.conf.urls import url
 
-from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView
+from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, AddFavView
 
 urlpatterns = [
     # 课程机构首页
@@ -12,4 +12,5 @@ urlpatterns = [
     url(r'^course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name='org_course'),
     url(r'^desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name='org_desc'),
     url(r'^teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name='org_teacher'),
+    url(r'^add_fav/$', AddFavView.as_view(), name='add_fav'),
 ]
