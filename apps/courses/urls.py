a6d1d69cc840f29792dc46fdabda762b9a0947c8
marshallstan <413428648@qq.com>
2019-03-24 23:03:12 +0800

    course comments and add comment

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,10 +1,12 @@
 # __author__ = 'Marshall Stan'
 
 from django.conf.urls import url
-from .views import CourseListView, CourseDetailView, CourseInfoView
+from .views import CourseListView, CourseDetailView, CourseInfoView, CommentsView, AddCommentsView
 
 urlpatterns = [
     url(r'^list/$', CourseListView.as_view(), name='course_list'),
     url(r'^detail/(?P<course_id>\d+)/$', CourseDetailView.as_view(), name='course_detail'),
     url(r'^info/(?P<course_id>\d+)/$', CourseInfoView.as_view(), name='course_info'),
+    url(r'^comment/(?P<course_id>\d+)/$', CommentsView.as_view(), name='course_comment'),
+    url(r'^add_comment/$', AddCommentsView.as_view(), name='add_comment'),
 ]
