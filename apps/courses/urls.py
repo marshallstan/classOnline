8f9a1eae89c9dde986e1e3fdda1a295af315f54e
marshallstan <413428648@qq.com>
2019-03-29 08:01:31 +0800

    video play

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,7 +1,7 @@
 # __author__ = 'Marshall Stan'
 
 from django.conf.urls import url
-from .views import CourseListView, CourseDetailView, CourseInfoView, CommentsView, AddCommentsView
+from .views import CourseListView, CourseDetailView, CourseInfoView, CommentsView, AddCommentsView, VideoPlayView
 
 urlpatterns = [
     url(r'^list/$', CourseListView.as_view(), name='course_list'),
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^info/(?P<course_id>\d+)/$', CourseInfoView.as_view(), name='course_info'),
     url(r'^comment/(?P<course_id>\d+)/$', CommentsView.as_view(), name='course_comment'),
     url(r'^add_comment/$', AddCommentsView.as_view(), name='add_comment'),
+    url(r'^video/(?P<video_id>\d+)/$', VideoPlayView.as_view(), name='video_play'),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -3,7 +3,7 @@ from django.views.generic.base import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 
-from .models import Course, CourseResource
+from .models import Course, CourseResource, Video
 from operation.models import UserFavorite, CourseComments, UserCourse
 from utils.mixin_utils import LoginRequiredMixin
 
@@ -32,6 +32,32 @@ class CourseListView(View):
             'all_courses': courses,
             'sort': sort,
             'hot_courses': hot_courses,
+        })
+
+
+class VideoPlayView(View):
+    def get(self, request, video_id):
+        video = Video.objects.get(id=int(video_id))
+        course = video.lesson.course
+        course.students += 1
+        course.save()
+        user_courses = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_courses:
+            user_course = UserCourse(user=request.user, course=course)
+            user_course.save()
+
+        user_courses = UserCourse.objects.filter(course=course)
+        user_ids = [user_course.user.id for user_course in user_courses]
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        course_ids = [user_course.course.id for user_course in all_user_courses]
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by('-click_nums')[:5]
+        all_resources = CourseResource.objects.filter(course=course)
+
+        return render(request, 'course-play.html', {
+            'course': course,
+            'course_resources': all_resources,
+            'relate_courses': relate_courses,
+            'video': video,
         })
 
 
@@ -67,6 +93,8 @@ class CourseDetailView(View):
 class CourseInfoView(LoginRequiredMixin, View):
     def get(self, request, course_id):
         course = Course.objects.get(id=int(course_id))
+        course.students += 1
+        course.save()
         user_courses = UserCourse.objects.filter(user=request.user, course=course)
         if not user_courses:
             user_course = UserCourse(user=request.user, course=course)
