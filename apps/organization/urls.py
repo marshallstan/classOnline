fb77b400f28dfea3e52296ff67847620792b73e5
marshallstan <413428648@qq.com>
2019-03-29 23:25:40 +0800

    teacher detail

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import url
 
 from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView,OrgDescView, OrgTeacherView, AddFavView
-from .views import TeacherListView
+from .views import TeacherListView, TeacherDetailView
 
 urlpatterns = [
     # 课程机构首页
@@ -15,4 +15,5 @@ urlpatterns = [
     url(r'^teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name='org_teacher'),
     url(r'^add_fav/$', AddFavView.as_view(), name='add_fav'),
     url(r'^teacher/list/$', TeacherListView.as_view(), name='teacher_list'),
+    url(r'^teacher/detail/(?P<teacher_id>\d+)/$', TeacherDetailView.as_view(), name='teacher_detail'),
 ]
